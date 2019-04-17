aa214fe1198745ce1e74873a59fd3e029686fe6e
George Apazidis <geoapa@gmail.com>
2019-04-17 14:43:36 -0700

    scroll issue fix

--- script.js
+++ script.js
@@ -1,27 +1,13 @@
 
 // mobile icon click
-function mobileToggleOnClick () {
-    $('.burg').click(function () {
+function navClicks () {
+    $('.burg, nav a').click(function () {
         
         // animate icon
         animateMobileIcon();
         
         // show or hide menu
         navBarShowHide();  
-    });
-}
-
-// nav click
-function navOnClick () {
-    $('nav a').click(function () {
-        // hide menu
-        navBarShowHide();
-
-        // scroll to section
-        smoothScroll();
-
-        // toggle mobile menu icon
-        animateMobileIcon();
     });
 }
 
@@ -35,25 +21,9 @@ function animateMobileIcon () {
     $('.burg').stop().toggleClass('burg-close');
 }
 
-// smooth scroll to sections
-function smoothScroll () {
-	$('a[href*="#"]:not([href="#"])').on('click', function(e){
-		e.preventDefault();
-		
-		if( $( $.attr(this, 'href') ).length > 0 ){
-			$('html, body').animate(
-			{
-				scrollTop: $( $.attr(this, 'href') ).offset().top
-			}, 'fast');
-		}
-		return false;
-	});
+
+function handleClicks() {
+    navClicks();
 }
 
-function handleButtons() {
-    mobileToggleOnClick();
-    navOnClick();
-    smoothScroll();
-}
-
-$(handleButtons);
+$(handleClicks);
