ecbf217ee84e6c2dee142c398622a7842477299c
George Apazidis <geoapa@gmail.com>
2019-04-21 22:50:49 -0700

    header animation for largest breakpoint, smooth scroll with offset, JS resize/scroll handling

--- script.js
+++ script.js
@@ -26,8 +26,117 @@ function animateMobileIcon () {
     $('.burg').stop().toggleClass('burg-close');
 }
 
+
+// variables for throttling
+let scrollTimeout;
+let throttle = 200;
+
+// define boolean for largest breakpoint to be used scroll listener event
+let largeBreakPoint = $(this).outerWidth() >= 768 ? true : false ;
+
+// throttle function for browser resize
+$(window).on('resize', function(){
+
+    //console.log('resize NO throttle');
+
+    if (!scrollTimeout) {
+        scrollTimeout = setTimeout(function () {
+
+            //console.log('resize WITH throttle');
+
+            // recalculate this variable
+            largeBreakPoint = $(this).outerWidth() >= 768 ? true : false ;
+
+            // run animations when browser is resized
+            animateHeader();
+
+            scrollTimeout = null;
+        }, throttle);
+    }
+});
+
+// throttle function for browser scroll
+$(window).on('scroll', function () {
+
+    //console.log('scroll NO throttle');
+
+    if (!scrollTimeout) {
+        scrollTimeout = setTimeout(function () {
+
+            //console.log('scroll WITH throttle');
+            
+            // run animations when browser is scrolled
+            animateHeader();
+
+            scrollTimeout = null;
+        }, throttle);
+    }
+});
+
+// function to animate the header/nav when the browser is scrolled down 80px only on largest breakpoint
+function animateHeader () {
+    if (largeBreakPoint == true) {
+        if ($(window).scrollTop() > 80) {
+
+            // if cover does not exist then add the following HTML/CSS classes to handle animations
+            if (!$('.navCover').length) {
+
+                // insert top white block above nav
+                $('body').prepend(`<div class="navCover"></div>`);
+                
+                // add class to animate header elements via CSS
+                $('header').addClass('animate');
+
+                // add nav logo
+                $('nav ul').prepend(`<img class="navLogo" src="images/george-apazidis-blue.png" alt="George Apazidis">`)
+
+                // add class to animate nav via CSS
+                // use setTimeout to allow logo to be added to the DOM first (previous line) then animate
+                 setTimeout(`$('nav').addClass('animate');`, 50);
+            }
+        }
+        else {
+            // if cover exists, then reset header/nav to original states
+            if ($('.navCover').length) {
+
+                // remove nav cover
+                $('.navCover').remove();
+
+                // animate header elements back to original locations
+                $('header, nav').removeClass('animate');
+
+                // after navLogo animation, remove from DOM
+                setTimeout(`$('.navLogo').remove();`, 300);
+            
+            }
+        }
+    }
+}
+
+// function for smooth scrolling
+function anchorClicks () {
+    $('a[href^="#"]').on('click',function (e) 
+    {
+        // e.preventDefault();
+
+        let target = this.hash,
+        $target = $(target);
+
+        // set offset of larg breakpoint to 60px, otherwise keep at 0
+        let offsetNum = (largeBreakPoint == true ) ?  60 :  0;
+
+        // offset scroll function
+        $('html, body')
+            .stop()
+            .animate({ 'scrollTop': $target.offset().top - offsetNum }, 900, 'swing', function () {
+                window.location.hash = target;
+            });
+    });
+}
+
 function handleClicks() {
     navClicks();
+    anchorClicks();
 }
 
 $(handleClicks);
