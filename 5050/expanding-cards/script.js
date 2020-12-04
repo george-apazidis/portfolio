3efc9e31fbd00594ca539a1a505c58219aa58103
geoapa <geoapa@gmail.com>
2020-12-04 00:47:34 -0500

    add project starter and expanding cards

---
+++ 5050/expanding-cards/script.js
@@ -0,0 +1,16 @@
+// bring in panels into node list var
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+    // console.log(panel);
+    panel.addEventListener('click', () => {
+        removeActiveClasses();
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach((panel) => {
+        panel.classList.remove('active');
+    })
+}
