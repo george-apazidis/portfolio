f6705ef0c6b4d561c7d2e96f0d5a7d268f5ff005
geoapa <geoapa@gmail.com>
2021-01-18 12:15:53 -0500

    chore(progress steps): add README and more comment code

---
+++ 5050/progress-steps/README.md
@@ -0,0 +1,15 @@
+# Progress Steps
+
+A Progress UI component with animation based on user clicks.  
+
+
+
+* JS
+    * click event listeners on buttons
+    * dynamic active progress bar based on total # of steps
+    * boolean logic to handle completed steps and add 'active' class
+    * disable buttons based on step #
+
+* CSS
+    * transitions for smooth effects
+    * flexbox for layout
--- 5050/progress-steps/script.js
+++ 5050/progress-steps/script.js
@@ -8,7 +8,10 @@ let currentActive = 1
 next.addEventListener('click', () => {
     currentActive++;
     
+
     if(currentActive > circles.length) {
+        /* if on the last step, do not increment currentActive.
+           Always keep it equal to the last step # */
         currentActive = circles.length;
     }
 
@@ -20,6 +23,8 @@ prev.addEventListener('click', () => {
     currentActive--;
     
     if(currentActive < 1) {
+        /* if on the first step, do not decrement currentActive.
+           Always keep it equal to the frist step  */
         currentActive = 1;
     }
     
@@ -27,6 +32,10 @@ prev.addEventListener('click', () => {
 })
 
 function update() {
+
+    /* For each circle, check if index of circle is less than currentActive. 
+       if true then add 'active', if false then remove 'active'
+    */
     circles.forEach((circle, idx) => {
         if(idx < currentActive) {
             circle. classList.add('active')
@@ -36,10 +45,13 @@ function update() {
         }
     })
 
+    /* create node list of all active steps */
     const actives = document.querySelectorAll('.active')
 
+    /* create percentage for progress width by using length of active steps node list and divide by length of circles node list */
     progress.style.width = ((actives.length -1) / (circles.length -1)) * 100 + '%';
 
+    /* disable buttons if step # is on first or last */
     if(currentActive === 1) {
         prev.disabled = true;
     } else if(currentActive === circles.length) {
