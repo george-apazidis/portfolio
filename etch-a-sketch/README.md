08a3cf698933c332326513a8efe5076cfa8c57f9
George Apazidis <geoapa@gmail.com>
2021-05-12 11:02:39 -0400

    chore(etch-a-sketch): add MD; clean up JS; add comment code

---
+++ etch-a-sketch/README.md
@@ -0,0 +1,21 @@
+# Etch-a-sketch game
+
+"Etch-a-sketch" is a game that allows the user to draw on a canvas using the keyboard arrow keys.  The user can clear the drawing by clicking on the 'Shake' button.
+
+
+# Demo
+
+* [Live Demo](https://www.geoapa.com/etch-a-sketch/)
+
+## Technology
+
+* HTML5 - canvas
+* CSS3 - animation, keyframes
+* JavaScript (ES6) - DOM elements, keyboard events, switch statements
+
+## Development Road Map
+
+Features for future iterations include:
+* create boundary around canvas for the drawing to 'stop', currently the drawing goes off the canvas
+* convert to responsive
+* add buttons to UK for arrow keys (up, down, left, right)
--- etch-a-sketch/etch-a-sketch.js
+++ etch-a-sketch/etch-a-sketch.js
@@ -1,79 +1,125 @@
-// Select the elements on the page - canvas, shake button
+//----- SELECT THE ELEMENTS ON THE PAGE -----
 const canvas = document.querySelector('#etch-a-sketch');
+
+// the context object is the place where we do our drawing
 const ctx = canvas.getContext('2d');
-const shakebutton = document.querySelector('.shake');
-const MOVE_AMOUNT = 50;
-// Setup our canvas for drawing
-// make a variable called height and width from the same properties on our canvas.
+
+const shakeBTN = document.querySelector('.shake');
+
+// distance in px to move the drawing
+const MOVE_AMOUNT = 20;
+
+
+//----- SETUP CANVAS FOR DRAWING -----
+
+// create variables called height and width from the same properties on our canvas (destructuring)
 const { width, height } = canvas;
 
+// create random x and y starting points on the canvas based on width/height of canvas
 let x = Math.floor(Math.random() * width);
 let y = Math.floor(Math.random() * height);
-// create random x and y starting points on the canvas
 
+// styles for drawing edge
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = MOVE_AMOUNT;
 
 let hue = 0;
 ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-ctx.beginPath(); // start the drawing
+
+// start the drawing
+ctx.beginPath();
+
+// starting point
 ctx.moveTo(x, y);
+
+// ending point
 ctx.lineTo(x, y);
+
+// draws line between starting and ending points (moveTo > lineTo)
+// on page load, these values will be the same values so that it renders a circle
 ctx.stroke();
 
-// write a draw function
-function draw({ key }) {
-  // increment the hue
-  hue += 1;
-  console.log(hue);
-  ctx.strokeStyle = `hsl(${Math.random() * 360}, 100%, 50%)`;
-  console.log(key);
-  // start the path
-  ctx.beginPath();
-  ctx.moveTo(x, y);
-  // move our x and y values depending on what the user did
-  switch (key) {
-    case 'ArrowUp':
-      y -= MOVE_AMOUNT;
-      break;
-    case 'ArrowRight':
-      x += MOVE_AMOUNT;
-      break;
-    case 'ArrowDown':
-      y += MOVE_AMOUNT;
-      break;
-    case 'ArrowLeft':
-      x -= MOVE_AMOUNT;
-      break;
-    default:
-      break;
-  }
-  ctx.lineTo(x, y);
-  ctx.stroke();
+
+//----- DRAW -----
+
+function draw(key) {
+
+    // increment the hue
+    hue += 5;
+
+    // update HSL value by changing the 'hue'
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+
+    // start the drawing
+    // necessary to retain color > all lines are separate paths
+    ctx.beginPath();
+
+    // starting point
+    ctx.moveTo(x, y);
+
+
+    // update the x and y values depending on what key was pressed
+    switch (key) {
+        case 'ArrowUp':
+            y -= MOVE_AMOUNT;
+            break;
+        case 'ArrowRight':
+            x += MOVE_AMOUNT;
+            break;
+        case 'ArrowDown':
+            y += MOVE_AMOUNT;
+            break;
+        case 'ArrowLeft':
+            x -= MOVE_AMOUNT;
+            break;
+        default:
+            break;
+    }
+    // ending point
+    ctx.lineTo(x, y);
+
+    // draws line between starting and ending points (moveTo > lineTo)
+    ctx.stroke();
 }
 
-// write a handler for the keys
+//----- KEY HANDLERS -----
+
 function handleKey(e) {
-  if (e.key.includes('Arrow')) {
-    e.preventDefault();
-    draw({ key: e.key });
-  }
+
+    // if key is any arrow key
+    if (e.key.includes('Arrow')) {
+
+        // prevent the page from scrolling
+        e.preventDefault();
+
+        // call the draw function
+        draw(e.key);
+    }
 }
-// clear /shke function
+
+//----- CLEAR CANVAS -----
+
 function clearCanvas() {
-  canvas.classList.add('shake');
-  ctx.clearRect(0, 0, width, height);
-  canvas.addEventListener(
-    'animationend',
-    function() {
-      console.log('Done the shake!');
-      canvas.classList.remove('shake');
-    },
-    { once: true }
-  );
+    
+    // shake class is used to handle animation
+    canvas.classList.add('shake');
+
+    // 
+    ctx.clearRect(0, 0, width, height);
+
+    // listen for CSS animation to finish
+    canvas.addEventListener('animationend', function() {
+
+        // after animation is complete, remove class shake
+        canvas.classList.remove('shake');
+        },
+
+        // third argument object 'once' to only add the event listener once
+        { once: true }
+    );
 }
 
 // listen for arrow keys
 window.addEventListener('keydown', handleKey);
-shakebutton.addEventListener('click', clearCanvas);
+shakeBTN.addEventListener('click', clearCanvas);
